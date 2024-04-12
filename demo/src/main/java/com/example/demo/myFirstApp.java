ac5e2e539a7daac14a5aa5404bfd8531da9844fd
Sachin <sachinkumbhar307@gmail.com>
2024-04-12 09:05:25 +0530

    1stProject12April

---
+++ demo/src/main/java/com/example/demo/myFirstApp.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myFirstApp {
+    @RequestMapping("/hello")
+    public String sayHello()
+    {
+        return "Hello World kgjkfjk";
+    }
+    @RequestMapping("/hello{name}")
+    public String printByName(@PathVariable("name") String pathvar)
+    {
+        return "Hello " + pathvar;
+    }
+}
+
---
+++ demo_spring1/src/main/java/com/example/spring1/DemoSpring1Application.java
@@ -0,0 +1,14 @@
+package com.example.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpring1Application.class, args);
+
+	}
+
+}
